fcbb0176729d414377818d53f85fd7a4cd92a5e0
Cristian.Martinello <cristian.martinello@workiva.com>
2017-06-26 16:21:37 +0200

    bump version

--- setup.py
+++ setup.py
@@ -1,20 +1,43 @@
-#!/usr/bin/env python
+from setuptools import find_packages, setup
 
-from setuptools import setup
+from pip.req import parse_requirements
 
-setup(name='DogPush',
-      version='0.3.4',
-      description='DogPush: manage datadog alerts in local files.',
-      author='Nadav S Samet',
-      author_email='thesamet@gmail.com',
-      license='Apache 2.0',
-      url='https://github.com/trueaccord/DogPush',
-      install_requires=[
-          'PyYAML>=3.11',
-          'datadog>=0.10.0',
-          'pytz>=2015.7',
-      ],
-      packages=['dogpush'],
-      scripts=['scripts/dogpush']
-      )
 
+def get_version():
+    import imp
+
+    mod = imp.load_source('_meta', 'dogpush/_meta.py')
+
+    return mod.version
+
+
+def get_requirements(filename):
+    try:
+        from pip.download import PipSession
+
+        session = PipSession()
+    except ImportError:
+        session = None
+
+    reqs = parse_requirements(filename, session=session)
+
+    return [str(r.req) for r in reqs]
+
+
+def get_install_requires():
+    return get_requirements('requirements.txt')
+
+
+setup_args = dict(
+    name='DogPush',
+    version=get_version(),
+    maintainer="InfRe Group",
+    maintainer_email="infre@workiva.com",
+    packages=find_packages(),
+    install_requires=get_install_requires(),
+    include_package_data=True,
+    scripts=['scripts/dogpush']
+)
+
+if __name__ == '__main__':
+    setup(**setup_args)
